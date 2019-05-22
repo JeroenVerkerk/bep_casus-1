7ab5f159e63e6c162378287087e6091ba84aeeb2
nicovdbrink <nicovdbrink@hotmail.com>
2019-05-22 11:44:23 +0200

    setup commit

---
+++ IEF converter/src/mongo/MongoSaver.java
@@ -0,0 +1,53 @@
+package mongo;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoSaver {
+	
+	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
+		String userName = "";
+		String password = "";
+		String database = "";
+		
+		MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
+		
+		boolean success = true;
+		
+		try (MongoClient mongoClient = new MongoClient(new ServerAddress("ds227939.mlab.com", 27939), credential, MongoClientOptions.builder().build()) ) {
+			
+			MongoDatabase db = mongoClient.getDatabase( database );
+			
+			MongoCollection<Document> c = db.getCollection("");
+			
+			Document  doc = new Document ("to", to)
+			        .append("", from)
+			        .append("", subject)
+			        .append("", text)
+			        .append("", html);
+			c.insertOne(doc);
+		} catch (MongoException mongoException) {
+			System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
+			mongoException.printStackTrace();
+			success = false;
+		}
+		
+		return success;
+ 		
+	}
+
+}
