ecc2a3e059a53f4b56ddfdef34d3b9f71e020115
Mkemp <mkemp63@gmail.com>
2019-06-20 16:53:20 +0200

    save locals before merge

--- IEF converter/src/test/java/CLIReader/ReadCLITest.java
+++ IEF converter/src/test/java/CLIReader/ReadCLITest.java
@@ -1,14 +1,14 @@
-package CLIReader;
+package java.CLIReader;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.convert.CLIReader.ReadCLI;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ReadCLITest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
