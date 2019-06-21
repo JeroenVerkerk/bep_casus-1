87eb77b21f21e11468026030e2522fb3d1b9809b
Mkemp <mkemp63@gmail.com>
2019-06-21 15:55:44 +0200

    Update ReadCLITest.java

--- src/test/java/CLIReader/ReadCLITest.java
+++ src/test/java/CLIReader/ReadCLITest.java
@@ -5,119 +5,130 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReadCLITest {
     private ReadCLI readCLI;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private final PrintStream originalErr = System.err;
+    private final OutputStream os = new ByteArrayOutputStream();
+    private final PrintStream ps = new PrintStream(os);
 
     @BeforeEach
     void setUpStreams() {
         readCLI = new ReadCLI();
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
+        System.setOut(ps);
     }
 
+
     @Test
-    void testReturnMonth(){
-        readCLI.returnMonth(2);
-        assertEquals("Februari\r\n", outContent.toString());
+    void testReturnMonth() {
+        ReadCLI.returnMonth(2);
+        assertEquals("Februari\r\n", os.toString());
     }
+
     @Test
     void testMonth1() {
         String[] args = {"1"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 1\r\n" +
-                "Januari\r\n", outContent.toString());
+                "Januari\r\n", os.toString());
     }
+
     @Test
     void testMonth2() {
         String[] args = {"2"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 2\r\n" +
-                "Februari\r\n", outContent.toString());
+                "Februari\r\n", os.toString());
     }
+
     @Test
     void testMonth3() {
         String[] args = {"3"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 3\r\n" +
-                "Maart\r\n", outContent.toString());
+                "Maart\r\n", os.toString());
     }
+
     @Test
     void testMonth4() {
         String[] args = {"4"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 4\r\n" +
-                "April\r\n", outContent.toString());
+                "April\r\n", os.toString());
     }
+
     @Test
     void testMonth5() {
         String[] args = {"5"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 5\r\n" +
-                "Mei\r\n", outContent.toString());
+                "Mei\r\n", os.toString());
     }
+
     @Test
     void testMonth6() {
         String[] args = {"6"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 6\r\n" +
-                "Juni\r\n", outContent.toString());
+                "Juni\r\n", os.toString());
     }
+
     @Test
     void testMonth7() {
         String[] args = {"7"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 7\r\n" +
-                "Juli\r\n", outContent.toString());
+                "Juli\r\n", os.toString());
     }
+
     @Test
     void testMonth8() {
         String[] args = {"8"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 8\r\n" +
-                "Augustus\r\n", outContent.toString());
+                "Augustus\r\n", os.toString());
     }
+
     @Test
     void testMonth9() {
         String[] args = {"9"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 9\r\n" +
-                "September\r\n", outContent.toString());
+                "September\r\n", os.toString());
     }
+
     @Test
     void testMonth10() {
         String[] args = {"10"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 10\r\n" +
-                "Oktober\r\n", outContent.toString());
+                "Oktober\r\n", os.toString());
     }
+
     @Test
     void testMonth11() {
         String[] args = {"11"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 11\r\n" +
-                "November\r\n", outContent.toString());
+                "November\r\n", os.toString());
     }
+
     @Test
     void testMonth12() {
         String[] args = {"12"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 12\r\n" +
-                "December\r\n", outContent.toString());
+                "December\r\n", os.toString());
     }
 
     @Test
     void testMultipleArguments() {
         String[] args = {"5", "6"};
         ReadCLI.main(args);
-        assertEquals("Zero or multiple arguments supplied, must be one\r\n", outContent.toString());
+        assertEquals("Zero or multiple arguments supplied, must be one\r\n", os.toString());
     }
 
     @Test
@@ -125,35 +136,33 @@ class ReadCLITest {
         String[] args = {"23"};
         ReadCLI.main(args);
         assertEquals("Finding data for month 23\r\n" +
-                "Error, 23 is not a valid month\r\n", outContent.toString());
+                "Error, 23 is not a valid month\r\n", os.toString());
     }
 
     @Test
     void testNotAnInt() {
         String[] args = {"string"};
         ReadCLI.main(args);
-        assertEquals("Argument string is not an integer\r\n", outContent.toString());
+        assertEquals("Argument string is not an integer\r\n", os.toString());
     }
 
     @Test
-    void notZeroArguments(){
+    void notZeroArguments() {
         String[] args = {};
         ReadCLI.main(args);
-        assertEquals("Zero or multiple arguments supplied, must be one\r\n", outContent.toString());
+        assertEquals("Zero or multiple arguments supplied, must be one\r\n", os.toString());
     }
 
     @Test
-    void notNull(){
+    void notNull() {
         String[] args = {};
         args = null;
         ReadCLI.main(args);
-        assertEquals("Null is not a month\r\n", outContent.toString());
+        assertEquals("Null is not a month\r\n", os.toString());
     }
 
 
     @AfterEach
     void restoreStreams() {
-        System.setOut(originalOut);
-        System.setErr(originalErr);
     }
 }
